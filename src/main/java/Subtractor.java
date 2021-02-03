75ed0856def8e31499e1eab50a33f0e28beead05
KaushikeePPal <kaushikee@gmail.com>
2021-02-03 22:01:07 +0530

    Subtractor class added

---
+++ src/main/java/Subtractor.java
@@ -0,0 +1,10 @@
+
+public class Subtractor {
+
+	public static void main(String[] args) {
+        
+	      int num1 = 5, num2 = 15,  subtract;	      
+        subtract = num2 - num1;	     
+	      System.out.println("Difference of these numbers: "+subtract);
+	   }
+}
